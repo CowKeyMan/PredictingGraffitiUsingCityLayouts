dc6df4538f339a0def1cda9a32064f6121f973ff
CowKeyMan <dancauchi1@gmail.com>
2021-09-30 22:45:41 +0300

    Refactor combine_lighting_and_graffiti, use geodesic distance for accuracy, Multithread for speed, Add some additional columns for later joining

--- data_cleaning/combine_lighting_and_graffiti.py
+++ data_cleaning/combine_lighting_and_graffiti.py
@@ -1,66 +1,112 @@
 import pandas as pd
-import numpy as np
 import json
-
-#Convert Geolocation to Tuple (x,y)
-def conv_to_Point(val):
-  val = json.loads(val)
-  (x,y) = (val['coordinates'])
-  return (x,y)
-
-#Find nearest coordinate
-def findNearest(row, df):
-  if isinstance(row[2], float):
-    (x0, y0) = row[0]
-    nearest = min(df['Geolocation'], key=lambda x:
-      (x[0] - x0)**2 + (x[1] - y0)**2)
-    lc = df[df['Geolocation'] == nearest]['Local Area'].item()
-    return (row[0], row[1], lc, row[3])
-  return row
-
-columns=['Geolocation', 'Type', 'Local Area', 'Count']
-
-#Load required csv:s
-graf = pd.read_csv("resources/data/graffiti.csv", sep=';')
-prop = pd.read_csv("resources/data/property-addresses.csv", sep=';')
-stlig = pd.read_csv("resources/data/street-lighting-poles.csv", sep=';')
-
-#Convert Geolocation
-graf['Geom'] = graf['Geom'].map(conv_to_Point)
-stlig['Geom'] = stlig['Geom'].map(conv_to_Point)
-prop['Geom'] = prop['Geom'].map(conv_to_Point)
-
-#Convert to same form [Geolocation | Type | Local Area | Count]
-graf = pd.DataFrame({'Geolocation': graf['Geom'], 'Type': 'G', 'Local Area': graf['Geo Local Area'], 'Count': graf['COUNT']})
-stlig = pd.DataFrame({'Geolocation': stlig['Geom'], 'Type': 'L', 'Local Area': stlig['Geo Local Area'], 'Count': stlig['NODE_NUMBER']})
-prop = pd.DataFrame({'Geolocation': prop['Geom'], 'Type': 'P', 'Local Area': prop['Geo Local Area'], 'Count': 1})
-
-#Fix missing values in Local Area
-#Graffiti
-nonNa = graf.dropna()
-mgraf = pd.DataFrame([
-  findNearest(row, nonNa) for row in zip(graf['Geolocation'], graf['Type'], graf['Local Area'], graf['Count'])
-], columns=columns)
-
-#Street Lightning
-nonNa = stlig.dropna()
-stlig = pd.DataFrame([
-  findNearest(row, nonNa) for row in zip(stlig['Geolocation'], stlig['Type'], stlig['Local Area'], stlig['Count'])
-], columns=columns)
-
-#Property
-nonNa = prop.dropna()
-prop = pd.DataFrame([
-  findNearest(row, nonNa) for row in zip(prop['Geolocation'], prop['Type'], prop['Local Area'], prop['Count'])
-], columns=columns)
+from geopy.distance import geodesic
+import multiprocessing
+from pathlib import Path
 
 
-#Combine fixed datasets & write to csv
-res = pd.concat([graf, stlig, prop])
-#graf.to_csv('F_graffiti.csv')
-#stlig.to_csv('F_propertyA.csv')
-#stlig.to_csv('F_streetL.csv')
-res['Latitude'] = [x[0] for x in res['Geolocation']]
-res['Longitude'] = [x[1] for x in res['Geolocation']]
-res.drop(columns=['Geolocation'], inplace=True)
-res.to_csv('resources/data/graffiti_combined.csv', index=False)
+def conv_to_point(val):
+    lon, lat = json.loads(val)['coordinates']
+    return lat, lon
+
+
+# find nearest coordinate
+def find_nearest(row: pd.Series, df_non_na: pd.DataFrame) -> str:
+    if not pd.isnull(row['Geo Local Area']):
+        return row['Geo Local Area']
+    return min(
+        df_non_na['Geom'],
+        key=lambda lat_lon: geodesic(lat_lon, row['Geom'])
+    )
+
+
+def chunk_find_nearest(
+    index: int,
+    df: pd.DataFrame,
+    df_non_na: pd.DataFrame,
+) -> None:
+    # df['Geo Local Area'] = [
+    #     find_nearest(row[1], df_non_na) for row in df.iterrows()
+    # ]
+    result = []
+    for i, row in enumerate(df.iterrows()):
+        if i % 10 == 0:
+            print(f'{index}: {i + 1}/{len(df)}')
+        result.append(find_nearest(row[1], df_non_na))
+    df['Geo Local Area'] = result
+    df.to_csv(f'resources/data/temp_{index}.csv', index=False)
+
+
+if __name__ == '__main__':
+    # load required csvs
+    graf = pd.read_csv("resources/data/graffiti.csv", sep=';')
+    prop = pd.read_csv("resources/data/property-addresses.csv", sep=';')
+    stlig = pd.read_csv("resources/data/street-lighting-poles.csv", sep=';')
+
+    # convert Geolocation
+    graf['Geom'] = graf['Geom'].map(conv_to_point)
+    stlig['Geom'] = stlig['Geom'].map(conv_to_point)
+    prop['Geom'] = prop['Geom'].map(conv_to_point)
+
+    # add type
+    graf['Type'] = 'G'
+    stlig['Type'] = 'L'
+    prop['Type'] = 'B'
+
+    # add Count column for each of the objects
+    stlig['Count'] = 1
+    prop['Count'] = 1
+    graf['Count'] = graf['COUNT']
+
+    # concatenate
+    df = pd.concat([graf, stlig, prop], ignore_index=True)
+
+    # get latitude and longitude
+    df['Latitude'] = [x[0] for x in df['Geom']]
+    df['Longitude'] = [x[1] for x in df['Geom']]
+
+    # drop unecessary columns
+    df = df[
+        'Geo Local Area,Count,SITE_ID,Latitude,Longitude,Geom,Type'.split(',')
+    ]
+
+    # fix missing values in Local Area - single threaded
+    # df['Geo Local Area'] = [
+    #     find_nearest(row[1], df[['Geom', 'Geo Local Area']].dropna())
+    #     for row in df.iterrows()
+    # ]
+
+    # fix missing values in Local Area - multi threaded
+    # setup multiprocessing parameters
+    jobs = 8
+    processes = []
+    df_non_na = df[['Geom', 'Geo Local Area']].dropna()
+    # shuffle to get (almost) equal workload
+    df = df.sample(frac=1, ignore_index=True)
+    # split dataframes into chunks and process them individually
+    chunk_size = len(df) / jobs
+    for j in range(jobs):
+        chunk = df.iloc[round(j * chunk_size): round((j + 1) * chunk_size)]
+        process = multiprocessing.Process(
+            target=chunk_find_nearest,
+            args=[j, chunk, df_non_na],
+        )
+        process.start()
+        processes.append(process)
+    for process in processes:
+        process.join()
+    # read the processed dataframes from disk
+    df = pd.concat(
+        [
+            pd.read_csv(f'resources/data/temp_{index}.csv')
+            for index in range(jobs)
+        ],
+        ignore_index=True
+    )
+    for index in range(jobs):
+        Path(f'resources/data/temp_{index}.csv').unlink()
+
+    # write result to csv
+    df.drop(columns=['Geom'], inplace=True)
+    df.sort_values('Type', inplace=True)
+    df.to_csv('resources/data/graffiti_combined.csv', index=False)
