10d5bd93d347635c9106bde5a332cd20c4bf78b4
CowKeyMan <dancauchi1@gmail.com>
2021-10-01 00:23:35 +0300

    Update graffiti vs light charts to include buildings as well

---
+++ charts/graffiti_vs_light.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Read CSV
+df = pd.read_csv('resources/data/graffiti_combined.csv')
+
+# Drop unecessary columns
+df = df[['Type', 'Geo Local Area', 'Count']]
+
+# Change both street lighting and buildings type to 'L'
+df['Type'].replace({'S': 'L', 'B': 'L'}, inplace=True)
+
+grouped_values = []
+# Group based on 'Geo Local Area' and count light sources and graffiti amount
+for area in df['Geo Local Area'].unique():
+    df_area = df[df['Geo Local Area'] == area]
+    light_amount = df_area[df_area['Type'] == 'L']['Count'].sum()
+    graffiti_amount = df_area[df_area['Type'] == 'G']['Count'].sum()
+    grouped_values.append([area, light_amount, graffiti_amount])
+df = pd.DataFrame(
+    grouped_values,
+    columns=['Geo Local Area', 'Light Sources', 'Graffiti Count']
+)
+
+# plot
+fig, scatter = plt.subplots(figsize=(10, 6), dpi=100)
+scatter \
+    = sns.regplot(x='Light Sources', y='Graffiti Count', data=df, fit_reg=True)
+plt.show()
