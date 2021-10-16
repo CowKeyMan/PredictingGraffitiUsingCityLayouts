b3d22f24e9ed94761f53f34040272782b9d1d627
Ari Barkhah <ari.barkhah@helsinki.fi>
2021-10-16 19:17:53 +0700

    Fixing Logit

--- analysis/Logres_Statsmodels.py
+++ analysis/Logres_Statsmodels.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import statsmodels.api as sm
+from utils import regularize_features
 
-fold_amount = 5
 continuous_variables = [
     'highest_elevation_m',
     'area_m2',
@@ -20,22 +20,17 @@ continuous_variables = [
     'geo_local_area_population',
     'pop_density',
 ]
-target_value = 'graffiti_count'
 
-df = pd.read_csv('resources/data/generated/buildings_model_features.csv')
-#print('df', df)
-#df.loc[df['graffiti_count']>1, 'graffiti_count']=1
-#df.loc[df['graffiti_count']<1, 'graffiti_count']=0
+df = pd.read_csv('buildings_model_features.csv')
 df['graffiti_count'] = (df['graffiti_count'] > 0).astype(float)
+df, _ = regularize_features(df, None, continuous_variables)
+y = df['graffiti_count']
+x = df.drop(columns=['graffiti_count'])
+x = x.drop(columns=df.iloc[:, 20:42])
 
-#print(df.to_string())
-xtrain = df[['highest_elevation_m', 'area_m2', 'sub_buildings','nearby_buildings_count','nearby_graffiti_count',
-             'nearby_graffiti_average','nearby_graffiti_buildings','nearby_buildings_average_height','nearby_buildings_median_height',
-             'nearby_buildings_total_sub_buildings','nearby_buildings_average_sub_buildings','nearby_buildings_median_sub_buildings',
-             'nearby_street_lights']]
-ytrain = df[['graffiti_count']]
-
-log_reg = sm.Logit(ytrain, xtrain).fit()
+print('x', x)
+print('y', y)
+log_reg = sm.Logit(y, x).fit()
 
 print('log_reg', log_reg)
 print('log_reg_summary', log_reg.summary())
