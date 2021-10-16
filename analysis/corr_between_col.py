50ce3e196091a9f3f928178210d75f29ef93e884
Ari Barkhah <ari.barkhah@helsinki.fi>
2021-10-16 13:23:47 +0700

    graffiti count as a dependent variable

--- analysis/corr_between_col.py
+++ analysis/corr_between_col.py
@@ -15,9 +15,8 @@ df.loc[df['graffiti_count']<1, 'graffiti_count']=0
 xtrain = df[['highest_elevation_m', 'area_m2', 'sub_buildings','nearby_buildings_count','nearby_graffiti_count',
              'nearby_graffiti_average','nearby_graffiti_buildings','nearby_buildings_average_height','nearby_buildings_median_height',
              'nearby_buildings_total_sub_buildings','nearby_buildings_average_sub_buildings','nearby_buildings_median_sub_buildings',
-             'nearby_street_lights','graffiti_count']]
-ytrain = df[['geo_local_area_Arbutus-Ridge']]
-
+             'nearby_street_lights']]
+ytrain = df[['graffiti_count']]
 log_reg = sm.Logit(ytrain, xtrain).fit()
 
 print('log_reg', log_reg)
