945ed303470b26c7b1952feb488db3dc00cb09d8
CowKeyMan <dancauchi1@gmail.com>
2021-10-19 11:13:08 +0300

    Add different tiers for nearby objects, depending on how far they are

--- data_cleaning/buildings_and_nearby_objects.py
+++ data_cleaning/buildings_and_nearby_objects.py
@@ -29,9 +29,16 @@ def euclidean(x1: float, y1: float, x2: float, y2: float):
     return np.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
 
 
-ref_point_1 = (49.278439, -123.091596)
-ref_point_2 = (49.279119, -123.091581)
-ref_distance = euclidean(*ref_point_1, *ref_point_2)
+initial_ref_point = (49.278439, -123.091596)
+one_house_away_ref_point = (49.278684, -123.091583)
+one_house_away_distance \
+    = euclidean(*initial_ref_point, *one_house_away_ref_point)
+
+name_to_distance = {
+    'one_house_away': one_house_away_distance,
+    'two_houses_away': one_house_away_distance * 2,
+    'four_houses_away': one_house_away_distance * 4,
+}
 
 # 5 is wikipedia suggestion for "individual trees, houses"
 # source: https://en.wikipedia.org/wiki/decimal_degrees
@@ -71,43 +78,43 @@ for i, building in enumerate(buildings):
         df_building['lat'],
         df_building['lon'],
     )
-    df_close_buildings = df_building[
-        (building_distances < ref_distance)
-        & (building_distances > 0)
-    ]
-    exists = len(df_close_buildings)
-    building |= {
-        'nearby_buildings_count': len(df_close_buildings),
-        'nearby_graffiti_count':
-            df_close_buildings['graffiti_count'].sum(),
-        'nearby_graffiti_average':
-            df_close_buildings['graffiti_count'].mean() if exists else 0,
-        'nearby_graffiti_buildings':
-            sum(df_close_buildings['graffiti_count'] > 0),
-        'nearby_buildings_average_height':
-            df_close_buildings['highest_elevation_m'].mean() if exists else 0,
-        'nearby_buildings_median_height':
-            df_close_buildings['highest_elevation_m'].median() if exists
+    for name, distance in name_to_distance.items():
+        np.set_printoptions(precision=20)
+        df_close_buildings = df_building[
+            (building_distances < distance)
+            & (building_distances > 1e-12)
+        ]
+        exists = len(df_close_buildings)
+        building |= {
+            f'{name}_buildings_count': len(df_close_buildings),
+            f'{name}_graffiti_count':
+                df_close_buildings['graffiti_count'].sum(),
+            f'{name}_graffiti_average':
+                df_close_buildings['graffiti_count'].mean() if exists else 0,
+            f'{name}_graffiti_buildings':
+                sum(df_close_buildings['graffiti_count'] > 0),
+            f'{name}_buildings_average_height':
+                df_close_buildings['highest_elevation_m'].mean() if exists
                 else 0,
-        'nearby_buildings_total_sub_buildings':
-            df_close_buildings['sub_buildings'].sum(),
-        'nearby_buildings_average_sub_buildings':
-            df_close_buildings['sub_buildings'].mean() if exists else 0,
-        'nearby_buildings_median_sub_buildings':
-            df_close_buildings['sub_buildings'].median() if exists else 0,
-    }
+            f'{name}_buildings_median_height':
+                df_close_buildings['highest_elevation_m'].median() if exists
+                    else 0,
+            f'{name}_buildings_total_sub_buildings':
+                df_close_buildings['sub_buildings'].sum(),
+            f'{name}_buildings_average_sub_buildings':
+                df_close_buildings['sub_buildings'].mean() if exists else 0,
+            f'{name}_buildings_median_sub_buildings':
+                df_close_buildings['sub_buildings'].median() if exists else 0,
+        }
 
-    lighting_distances = euclidean(
-        building['lat'],
-        building['lon'],
-        df_lighting['lat'],
-        df_lighting['lon'],
-    )
-    df_close_lighting = df_lighting[
-        (lighting_distances < ref_distance)
-        & (lighting_distances > 0)
-    ]
-    building |= {'nearby_street_lights': len(df_close_lighting)}
+        lighting_distances = euclidean(
+            building['lat'],
+            building['lon'],
+            df_lighting['lat'],
+            df_lighting['lon'],
+        )
+        df_close_lighting = df_lighting[lighting_distances < distance]
+        building |= {f'{name}_street_lights': len(df_close_lighting)}
 
 pd.DataFrame(buildings).to_csv(
     'resources/data/generated/buildings_and_nearby_objects.csv', index=False
